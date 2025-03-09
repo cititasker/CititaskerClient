3a25c190150bb4382ca81c4936f3de2375fdc828
Taiwo Joseph <57369232+jtaiwo09@users.noreply.github.com>
2025-03-09 21:34:10 +0100

    quick fixes

--- src/app/(waitlist)/waitlist/page.tsx
+++ src/app/(waitlist)/waitlist/page.tsx
@@ -1,27 +1,9 @@
-"use client";
-// import Faq from "@/components/Faq";
-// import Footer from "@/components/waitlist/Footer";
-// import TaskerBanner from "@/components/waitlist/TaskerBanner";
-// import WhyCitiTasker from "@/components/waitlist/why/WhyCitiTasker";
-// import Hero from "@/components/waitlist/Hero/Hero";
 import Navbar from "@/components/waitlist/Navbar";
-// import dynamic from "next/dynamic";
-
-// const CountDownTimer = dynamic(
-//   () => import("@/components/waitlist/CountDownTimer"),
-//   { ssr: false }
-// );
 
 export default function Home() {
   return (
     <main>
       <Navbar />
-      {/* <Hero /> */}
-      {/* <WhyCitiTasker />
-      <CountDownTimer />
-      <Faq />
-      <TaskerBanner />
-      <Footer /> */}
     </main>
   );
 }
