aa5ff9f4f87609caae8e4c8c3dafa7c71baec570
Taiwo Joseph <57369232+jtaiwo09@users.noreply.github.com>
2025-03-09 20:19:15 +0100

    deployment fix

--- src/app/(waitlist)/waitlist/page.tsx
+++ src/app/(waitlist)/waitlist/page.tsx
@@ -1,27 +1,27 @@
 "use client";
-import Faq from "@/components/Faq";
-import Footer from "@/components/waitlist/Footer";
-import TaskerBanner from "@/components/waitlist/TaskerBanner";
-import WhyCitiTasker from "@/components/waitlist/why/WhyCitiTasker";
+// import Faq from "@/components/Faq";
+// import Footer from "@/components/waitlist/Footer";
+// import TaskerBanner from "@/components/waitlist/TaskerBanner";
+// import WhyCitiTasker from "@/components/waitlist/why/WhyCitiTasker";
 import Hero from "@/components/waitlist/Hero/Hero";
 import Navbar from "@/components/waitlist/Navbar";
-import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
 
-const CountDownTimer = dynamic(
-  () => import("@/components/waitlist/CountDownTimer"),
-  { ssr: false }
-);
+// const CountDownTimer = dynamic(
+//   () => import("@/components/waitlist/CountDownTimer"),
+//   { ssr: false }
+// );
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <Hero />
-      <WhyCitiTasker />
+      {/* <WhyCitiTasker />
       <CountDownTimer />
       <Faq />
       <TaskerBanner />
-      <Footer />
+      <Footer /> */}
     </main>
   );
 }
