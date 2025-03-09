c7262559c7d1c3966a7644dc82c1c149ae712538
Taiwo Joseph <57369232+jtaiwo09@users.noreply.github.com>
2025-03-09 21:05:31 +0100

    quick fix

--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -1,21 +1,13 @@
 "use client";
 import React, { Suspense } from "react";
 import { Provider } from "react-redux";
-import { persistor, store } from "@/store";
-import { PersistGate } from "redux-persist/integration/react";
-import ServerProvider from "./ServerProvider";
+import { store } from "@/store";
 import Loader from "@/app/components/reusables/Loading";
 
 const AppProvider = ({ children }: any) => {
   return (
     <Suspense fallback={<Loader />}>
-      <ServerProvider>
-        <Provider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
-            {children}
-          </PersistGate>
-        </Provider>
-      </ServerProvider>
+      <Provider store={store}>{children}</Provider>
     </Suspense>
   );
 };
