fdde6ec5766e391b48d25a4d44e3c39a80bb874d
Taiwo Joseph <57369232+jtaiwo09@users.noreply.github.com>
2025-03-09 13:44:07 +0100

    removed main web pages

--- src/app/(website)/(noFooter)/dashboard/notification/page.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Page() {
-  return <div> Notication page</div>;
-}
--- src/app/(website)/(noHeaderFooter)/post-task/loading.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import Loader from "@/components/reusables/Loading";
-import React from "react";
-
-export default function Loading() {
-  return <Loader />;
-}
