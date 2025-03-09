b8356d16fc9fdc6915170cc53d59cb5d13390aae
Taiwo Joseph <57369232+jtaiwo09@users.noreply.github.com>
2025-03-09 22:37:27 +0100

    qk fix

--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -1,13 +1,6 @@
 "use client";
 import React, { Suspense } from "react";
-// import { SnackbarProvider } from "./SnackbarProvider";
-// import { StyledEngineProvider, ThemeProvider } from "@mui/material";
-// import theme from "./theme";
-// import { persistor, store } from "@/store";
-import Loader from "@/components/reusables/Loading";
-// import { Provider } from "react-redux";
-// import { PersistGate } from "redux-persist/integration/react";
 
 export default function AppProvider({ children }: any) {
-  return <Suspense fallback={<Loader />}>{children}</Suspense>;
+  return <Suspense fallback={<p>Loading...</p>}>{children}</Suspense>;
 }
