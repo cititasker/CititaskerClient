e75d3a4b1e7a278703f276f00d6b3b9664a947b6
Taiwo Joseph <57369232+jtaiwo09@users.noreply.github.com>
2025-03-09 21:01:46 +0100

    quick fix

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,31 +1,12 @@
 "use client";
 import React from "react";
-import { SnackbarProvider } from "./SnackbarProvider";
 import { StyledEngineProvider, ThemeProvider } from "@mui/material";
-// import { useSession } from "next-auth/react";
-// import { useQuery } from "@tanstack/react-query";
-// import { getUserQuery } from "@/queries/user";
-// import { useAppDispatch } from "@/store/hook";
-// import { setUser } from "@/store/slices/user";
 import theme from "./theme";
 
 const Providers = ({ children }: any) => {
-  // const { data: session } = useSession();
-  // const dispatch = useAppDispatch();
-
-  // const { data } = useQuery(getUserQuery({ enabled: !!session }));
-
-  // useEffect(() => {
-  //   if (data) {
-  //     dispatch(setUser(data.data));
-  //   }
-  // }, [data]);
-
   return (
     <ThemeProvider theme={theme}>
-      <SnackbarProvider>
-        <StyledEngineProvider injectFirst>{children}</StyledEngineProvider>
-      </SnackbarProvider>
+      <StyledEngineProvider injectFirst>{children}</StyledEngineProvider>
     </ThemeProvider>
   );
 };
