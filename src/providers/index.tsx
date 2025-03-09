51c34c1020f57dd7dcf5949a3c913f5cb175fc8a
Taiwo Joseph <57369232+jtaiwo09@users.noreply.github.com>
2025-03-09 22:17:09 +0100

    quick fixes

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -3,26 +3,20 @@ import React, { Suspense } from "react";
 import { SnackbarProvider } from "./SnackbarProvider";
 import { StyledEngineProvider, ThemeProvider } from "@mui/material";
 import theme from "./theme";
-import { persistor, store } from "@/store";
+// import { persistor, store } from "@/store";
 import Loader from "@/components/reusables/Loading";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
+// import { Provider } from "react-redux";
+// import { PersistGate } from "redux-persist/integration/react";
 
 const Providers = ({ children }: any) => {
   return (
-    <Suspense fallback={<Loader />}>
-      <ThemeProvider theme={theme}>
-        <SnackbarProvider>
-          <StyledEngineProvider injectFirst>
-            <Provider store={store}>
-              <PersistGate loading={null} persistor={persistor}>
-                {children}
-              </PersistGate>
-            </Provider>
-          </StyledEngineProvider>
-        </SnackbarProvider>
-      </ThemeProvider>
-    </Suspense>
+    <ThemeProvider theme={theme}>
+      <SnackbarProvider>
+        <StyledEngineProvider injectFirst>
+          <Suspense fallback={<Loader />}>{children}</Suspense>
+        </StyledEngineProvider>
+      </SnackbarProvider>
+    </ThemeProvider>
   );
 };
 
