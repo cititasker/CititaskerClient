f722ba3be717aa189d08368f803e3faeba029f7a
Taiwo Joseph <57369232+jtaiwo09@users.noreply.github.com>
2025-03-09 22:41:38 +0100

    final fix

--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -1,6 +1,17 @@
 "use client";
 import React, { Suspense } from "react";
+// import { SnackbarProvider } from "./SnackbarProvider";
+// import { StyledEngineProvider, ThemeProvider } from "@mui/material";
+// import theme from "./theme";
+// import { persistor, store } from "@/store";
+import dynamic from "next/dynamic";
+// import { Provider } from "react-redux";
+// import { PersistGate } from "redux-persist/integration/react";
 
-export default function AppProvider({ children }: any) {
-  return <Suspense fallback={<p>Loading...</p>}>{children}</Suspense>;
+const Loader = dynamic(() => import("@/components/reusables/Loading"), {
+  ssr: false,
+});
+
+export default function Providers({ children }: any) {
+  return <Suspense fallback={<Loader />}>{children}</Suspense>;
 }
