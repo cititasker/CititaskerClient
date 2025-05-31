5f97bd6cfbf7f9cfdd39b67c7a5318ac2e03b8a1
Taiwo Joseph <57369232+jtaiwo09@users.noreply.github.com>
2025-05-31 10:07:58 +0100

    quick fixes

--- next.config.ts
+++ next.config.ts
@@ -9,18 +9,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  // async redirects() {
-  //   if (process.env.NODE_ENV === "production") {
-  //     return [
-  //       {
-  //         source: "/",
-  //         destination: "/waitlist",
-  //         permanent: false,
-  //       },
-  //     ];
-  //   }
-  //   return [];
-  // },
   eslint: {
     ignoreDuringBuilds: true,
   },
